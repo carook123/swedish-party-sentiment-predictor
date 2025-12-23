6a5409aeba1afb2a640cf33c4ac580607e4bdaba
sofiemelander <melander.sofie@hotmail.com>
2025-12-23 13:29:16 +0100

    added comment

--- data/get_scb_data.py
+++ data/get_scb_data.py
@@ -3,7 +3,14 @@ import pandas as pd
 import json
 
 def fetch_scb_data(url, name, filename, query, aggregate):
+    """
+    Fetches time series data from SCB API using a POST request and converts
+    the response to a pandas DataFrame with a standardized YYYY-MM date format.
 
+    If multiple entries exist for the same month and aggregation is enabled,
+    values are summed per month. The resulting DataFrame is sorted in
+    chronological order and saved as a CSV file.
+    """
     r = requests.post(url, json=query)
     r.raise_for_status()
     data = r.json()["data"]
@@ -22,7 +29,7 @@ def fetch_scb_data(url, name, filename, query, aggregate):
         .astype(str)
     )
 
-    if aggregate: # Summera om samma månad förekommer flera gånger
+    if aggregate: 
         df = (
             df
             .groupby("date", as_index=False)
@@ -41,8 +48,6 @@ with open("data/scb_metrics.json", "r", encoding="utf-8") as f:
     metrics = json.load(f)
 
 
-# All metrics and their query
-
 for metric in metrics:
     fetch_scb_data(
         url=metric["url"],
@@ -51,12 +56,3 @@ for metric in metrics:
         query=metric["query"],
         aggregate= metric["aggregate"]
     )
-
-# KPI - fastställda tal
-# Statsskuld - total alla markander
-# Arbetslöshet AKU - procent, Icke säsongrensad, Total män och kvinnor, Totalt 15-74 år	
-# Pänningmängd - Tillväxttakt 12-månaders, procent efter penningmängdsmått och månad
-# Bolåneränta (nyckeltal) - genomsnitt, procentssats
-# Elanvändning - summa förburkad el
-# Befolkning1 - totalt år 2025
-# Befolkning2 - totalt år 2000-2024
